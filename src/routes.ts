e7930c7eadd99264e42db7cf6db80ef9fc9eb5e4
Wellison Santos <silva.s.wellison@gmail.com>
2022-07-11 18:22:56 -0300

    FindArrobasServices

--- src/routes.ts
+++ src/routes.ts
@@ -10,9 +10,16 @@ router.get('/arroba', async (request, response) => {
 
   const user = await createUser.execute(url)
 
+  const arrobas = {
+    aleatorioArroba: user[Math.floor(Math.random() * user.length)],
+    maisArroba: user[Math.floor(Math.random() * user.length)]
+  }
+
   console.log(user)
 
-  return response.status(201).json(user)
+  console.log(typeof user)
+
+  return response.status(201).json(arrobas)
 })
 
 export { router }
--- src/services/FindArrobasServices.ts
+++ src/services/FindArrobasServices.ts
@@ -6,15 +6,19 @@ interface Returno {
 }
 
 export class FindArrobasServices {
-  public async execute(url: string): Promise<Returno> {
+  public async execute(url: string): Promise<string[]> {
     async function loadMore(page: puppeteer.Page, selector: string) {
       const moreButton = await page.$(selector)
       if (moreButton) {
-        console.log('seletor existe')
+        console.log('teste seletor existe')
         await page.click(selector)
-        await page.waitForSelector(selector, { timeout: 3000 })
+        await page
+          .waitForSelector(seletor, { timeout: 5000 })
+          .catch(() => console.log('timeout dentro'))
         await loadMore(page, selector)
-      } else console.log('seletor nao existe')
+      } else {
+        console.log('seletor nao existe')
+      }
     }
 
     async function getComments(page: puppeteer.Page, selector: string) {
@@ -29,24 +33,24 @@ export class FindArrobasServices {
     await page.goto(url)
     await page.screenshot({ path: 'example.png' })
     await page.waitForSelector('input[name="username"]')
-    await page.type('input[name="username"]', 'wellison.s.hatake@gmail.com')
+    await page.type('input[name="username"]', 'well.s.2022')
     await page.type('input[name="password"]', 'Luciana01251307')
     await page.click('button[type="submit"]')
     await page.waitForSelector('.JErX0 button[type="button"]')
     await page.click('.JErX0 button[type="button"]')
 
-    await loadMore(
-      page,
+    const seletor =
       '._ab8w._ab94._ab99._ab9h._ab9m._ab9p._abcj button[type="button"'
-    )
+
+    await page
+      .waitForSelector(seletor, { timeout: 5000 })
+      .catch(() => console.log('timeout'))
+
+    await loadMore(page, seletor)
+
     const comments = await getComments(page, '._a9zr span a')
     console.log(comments)
 
-    const arrobas = {
-      aleatorioArroba: comments[Math.floor(Math.random() * comments.length)],
-      maisArroba: comments[Math.floor(Math.random() * comments.length)]
-    }
-
-    return arrobas
+    return comments
   }
 }
